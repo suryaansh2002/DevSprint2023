0ed9e1d7ff42b0c1be5f773deeea5db435f78b46
Suryaansh_Rathinam <suryaansh@greedygame.com>
2023-02-19 13:58:56 +0530

    Working

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import axios from 'axios';
 
 export default function Home() {
-  return (
-    <div>
-    <Navbar/>
-    
-    </div>
-  )
+    const url='https://newsdata.io/api/1/news?apikey=pub_1749158b4447894e481fefd089da75e713eda'
+    useEffect(() => {
+        axios.get(url+'&lang=en').then((res) => {
+            console.log(res.data.results)
+        })
+    }, [])
+    return (
+        <div>
+            <Navbar />
+
+        </div>
+    )
 }
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
 export default function Login() {
+     const [email, setEmail] = useState('')
+     const [password, setPassword] = useState('')
+     const [error, setError] = useState('')
+     const [errorMsg, setErrorMsg] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(!email){
+            setError(true)
+            setErrorMsg('Please enter email')            
+        }
+        else if (email.indexOf('@')==-1){
+            setError(true)
+            setErrorMsg('Email address should contain @')
+        }
+        else if(password.length<8){
+            setError(true)
+            setErrorMsg('Password should be atleast 8 characters')
+        }
+        else if(password == password.toLowerCase()){
+            setError(true)
+            setErrorMsg('Password should contain atleast 1 upper case character')
+        }
+        else if(password == password.toUpperCase()){
+            setError(true)
+            setErrorMsg('Password should contain atleast 1 lower case character')
+        }
+        else if(!error){
+            window.location.href='/home'
+        }
+    }
     return (
         <div className='login-container'>
 
-            <form>
+            <form className='login-form'>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <input type="email" onChange={(e)=>{setEmail(e.target.value)}} class="form-control" id="exampleInputEmail1" required aria-describedby="emailHelp" placeholder="Enter email" />
+                    {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="password" onChange={(e)=>{setPassword(e.target.value)}} class="form-control" id="exampleInputPassword1" required placeholder="Password" />
                 </div>
-                <div class="form-check">
+                {/* <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                     <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                </div> */}
+                <button  class="btn login-sub" onClick={(e)=>{handleSubmit(e)}}>Submit</button>
+                {error &&
+                <div className='login-err'>{errorMsg}</div>
+                }
             </form>
         </div>
     )
