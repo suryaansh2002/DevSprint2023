a01c0494bd56bbe30b0c8ddc1e7ed1b31bd91cf6
Suryaansh_Rathinam <suryaansh@greedygame.com>
2023-02-19 14:53:46 +0530

    API Work

---
+++ src/Components/SideNav/Sidenav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Sidenav.css'
+export default function Sidenav() {
+  return (
+    <div className='sidenav'>
+        
+    </div>
+  )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,100 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import axios from 'axios';
-
+import Sidenav from '../../Components/SideNav/Sidenav';
+import './Home.css'
 export default function Home() {
-    const url='https://newsdata.io/api/1/news?apikey=pub_1749158b4447894e481fefd089da75e713eda'
+    const [articles, setArticles] = useState([])
+    var url = 'https://newsdata.io/api/1/news?apikey=pub_1749158b4447894e481fefd089da75e713eda'
+    
+    var url2='https://newsdata.io/api/1/news?apikey=pub_1749158b4447894e481fefd089da75e713eda&page=16767944382f68b51653791e2e6020068f413d764f'
+    var nextpage;
     useEffect(() => {
-        axios.get(url+'&lang=en').then((res) => {
-            console.log(res.data.results)
+        axios.get(url2 + '&language=en').then((res) => {
+            console.log(res.data)
+            const arr=[]
+            res.data.results.map((item)=>{
+                if(item.image_url){
+                    arr.push(item)
+                }
+            })
+            nextpage=res.data.nextpage;
+            url2=url+'&page='+nextpage;
+            if(arr.length<15){
+                  axios.get(url2 + '&language=en').then((response) => {
+                    response.data.results.map((item)=>{
+                        if(item.image_url){
+                            arr.push(item)
+                        }
+                    })
+                    nextpage=res.data.nextpage;
+                    url2=url+'&page='+nextpage; 
+                    console.log(arr)   
+                    if(arr.length<15){
+                        axios.get(url2 + '&language=en').then((response2) => {
+                          response2.data.results.map((item)=>{
+                              if(item.image_url){
+                                  arr.push(item)
+                              }
+                          })
+                          nextpage=res.data.nextpage;
+                          url2=url+'&page='+nextpage;
+                    console.log(arr)   
+
+                          if(arr.length<15){
+                            axios.get(url2 + '&language=en').then((response3) => {
+                              response3.data.results.map((item)=>{
+                                  if(item.image_url){
+                                      arr.push(item)
+                                  }
+                              })
+                              nextpage=res.data.nextpage;
+                              url2=url+'&page='+nextpage;    
+                              if(arr.length<15){
+                                axios.get(url2 + '&language=en').then((response4) => {
+                                  response4.data.results.map((item)=>{
+                                      if(item.image_url){
+                                          arr.push(item)
+                                      }
+                                  })
+                                  nextpage=res.data.nextpage;
+                                  url2=url+'&page='+nextpage;                                
+                              })
+                              console.log(arr)
+                          }
+                                          
+                          })
+                          console.log(arr)
+                      }
+                                          
+                      })
+                      console.log(arr)
+                  }
+                                  
+                })
+                console.log(arr)
+            }
+      setArticles(arr)
         })
     }, [])
     return (
-        <div>
-            <Navbar />
+        <div className='home-container'>
+            <div className='sidenav'>
+                <div>Filter By:</div>
 
+            </div>
+            <div className='home-main'>
+                {
+                    articles.length>0 && articles.map((item) => (<div className='news-card'>
+                        <div>
+                            <img src={item.image_url} className='news-img'/>
+                        </div>
+                        <div>{item.title}</div>
+                    </div>)
+                    )
+                }
+            </div>
         </div>
+
     )
 }
